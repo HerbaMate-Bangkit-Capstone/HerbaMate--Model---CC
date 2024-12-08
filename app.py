ade27866e0fba157a986bafafc67dd7a34ddb424
Nadya Dewindra <nadyadewindra@gmail.com>
2024-12-08 23:10:21 +0800

    Response result revision

--- app.py
+++ app.py
@@ -1,39 +1,51 @@
-from flask import Flask, request, jsonify
+from flask import Flask, render_template, request, jsonify
 import pandas as pd
 
 app = Flask(__name__)
 
-# Membaca dataset CSV
 df = pd.read_csv('dataset.csv')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/herb/predict', methods=['POST'])
 def recommend_plants():
     selected_symptoms = request.json.get('symptoms', [])
 
     if not selected_symptoms:
-        return jsonify({'error': 'No symptoms provided'}), 400
+        return jsonify({
+            'code': 400,
+            'message': 'No symptoms provided',
+            'data': None
+        }), 400
 
     recommended_plants = []
 
-    # Loop melalui setiap baris di dataset dan periksa apakah gejala cocok
     for index, row in df.iterrows():
         plant_symptoms = row['symptoms'].split(',')
         if any(symptom in plant_symptoms for symptom in selected_symptoms):
             recommended_plants.append({
-                'tanaman': row['herbs'],
-                'cara_pembuatan': row['usage_method'],
-                'gejala': plant_symptoms,
-                'relevance_score': row['relevance_score']
+                'herbs': row['herbs'],
+                'latin_name': row['latin_name'],
+                'symptoms': plant_symptoms,
+                'usage_method': row['usage_method']
             })
 
     if not recommended_plants:
-        return jsonify({'message': 'No plants match the selected symptoms'}), 404
+        return jsonify({
+            'code': 404,
+            'message': 'No plants match the selected symptoms',
+            'data': None
+        }), 404
 
-    # Mengurutkan berdasarkan relevance_score secara menurun
-    recommended_plants.sort(key=lambda x: x['relevance_score'], reverse=True)
+    recommended_plants.sort(key=lambda x: x['latin_name'], reverse=True)
 
-    return jsonify({'recommended_plants': recommended_plants})
-
+    return jsonify({
+        'code': 200,
+        'message': 'Successfully recommended plants',
+        'data': recommended_plants
+    }), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
